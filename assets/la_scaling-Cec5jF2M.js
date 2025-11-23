96a51717c43307b383fcf7b8b17eb1f69f3c9dd6
DavideWiest <DavideWiest@users.noreply.github.com>
2025-11-23 11:09:05 +0000

    deploy: 6d8b0822556e0b02104c3121111b5c4bae58194b

---
+++ assets/la_scaling-Cec5jF2M.js
@@ -0,0 +1,33 @@
+const e=`---
+slug: learning-algorithms-scalability
+title: Scalability of Learning Algorithms
+category: ideas
+summary: Informal findings made during the analysis of scalability  
+cover_image: /work/neural-architecture.jpg
+use_cover_as_card_bg: true
+show_cover_in_detail: false
+external_url: https://axym.org/files/TRL.pdf
+# main_points:
+#   - [atom, local Learning Rule]
+#   - [microchip, Online-Compatible]
+#   - [brain, Self-Supervised Learning]
+draft: false
+date: 2025-11-22
+rank: 2
+# links:
+#   - label: Repository
+#     url: https://github.com/axym-labs/trl
+---
+
+This post summarizes key findings made during the analysis of the scaiability of various learning algorithms, *including* the ones that are not specifically related or relevant.
+
+### Generating Data from an MLP
+- Data was generated from an untrained, frozen MLP with linear layers and ReLU activation function that receives a signal as input. This signal is a one-hot encoding or sample of a normal distribution. Its output is the input to a MLP that is trained with the different learning algorithms. 
+- The trained MLP essentially has to reconstruct the input signal (analogous to a latent variable) that constructed its inputs. The setup can be described as an Autoencoder where only the decoder is trained. The encoder and decoder have the same depth and layer dimensions that are reversed to each other (they are mirror layouts of each other).
+- We tested regression (reconstruction of the sampled signal) and classification (argmax signal = argmax output). Unsuprisingly, regression is much harder than classification.
+- Noise in the training data actually improves performance for classification. Probably because it enlargenes the subspace that is mapped to one class.
+- **The repeating pattern has been this**: Learning algorithms perform near equally for small encoder-MLPs. For larger/deeper ones, feedback alignment performs worst, backprop and direct feedback alignment (DFA) are in the middle and difference target propagation actually performs best. For a 4-layer MLP with layer output dimensions [512,512,512,1024], it achieves 19.4% accuracy while backprop achieves only 10.2%. This is likely because target-propagation trains the network to be a good reverse of the untrained encoder. It can better reverse the data-generating process by propagating *targets*, which correspond to the activations of an encoders previous layer, closer to the true input signal.
+- This is strong evidence that generating synthetic data with MLPs does not capture the effect we want - which is that alternative algorithms are outperformed by backprop for large-scale real data.
+- Using ReLU is problematic, especially for classification, because it is piecewise linear, meaning that for positive activations, the upstream layers are revealed, which principally makes creating a perfect inverse just a matter of data.
+- 
+`;export{e as default};
